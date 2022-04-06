8355495a806dcb24a7c215d4641c69a11b1cade8
White_Bear-2501 <taolanhuthedo2001@gmail.com>
2022-04-06 23:26:43 +0700

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./about.css";
-import ME from "../../assets/rodrick.jpg";
-import { AiTwotoneExperiment, AiFillFolderOpen } from "react-icons/ai";
-import { GiSpookyHouse } from "react-icons/gi";
+// import ME from "../../assets/rodrick.jpg";
+// import { AiTwotoneExperiment, AiFillFolderOpen } from "react-icons/ai";
+// import { GiSpookyHouse } from "react-icons/gi";
 
 const About = () => {
   return (
