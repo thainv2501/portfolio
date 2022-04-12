e66372d9eb2ab9faffb8a12616e3df85739abf3b
White_Bear-2501 <taolanhuthedo2001@gmail.com>
2022-04-12 11:00:14 +0700

    projects

--- src/components/myProjects/MyProjects.jsx
+++ src/components/myProjects/MyProjects.jsx
@@ -1,8 +1,45 @@
 import "./myProjects.css";
 import React from "react";
+import ME from "../../assets/rodrick.jpg";
 
 const MyProjects = () => {
-  return <div id="myProjects">MyProjects</div>;
+  return (
+    <div id="myProjects">
+      <div className="container myProjects__container">
+        <div className="myProjects__title">
+          <h5>You could view some of my</h5>
+          <h1>Projects</h1>
+        </div>
+
+        <div className="projects">
+          <section className="project">
+            <div className="project__img">
+              <img src={ME} alt="" />
+            </div>
+
+            <div className="detail__top">
+              <h1 className="project__name">check1</h1>
+              <p className="description">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Facilis possimus alias quo at excepturi sed aliquid aperiam
+                dolorem. Hic ab voluptate aperiam quam? Omnis unde molestiae
+                asperiores ratione dolorem necessitatibus.
+              </p>
+            </div>
+
+            <div className="detail__bottom">
+              <a href="#home" className="btn">
+                Link Code
+              </a>
+              <a href="#home" className="btn btn-primary">
+                View demo
+              </a>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyProjects;
