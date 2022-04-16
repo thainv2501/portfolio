c775fd78ff61410798641e20cf03044c80d8a537
White_Bear-2501 <taolanhuthedo2001@gmail.com>
2022-04-16 21:34:00 +0700

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,40 +14,44 @@ const About = ({ about }) => {
       </div>
 
       <div className="container about__container">
-        <div className="about__me">
-          <div className="about__me-img">
-            <img src={ME} alt="About Me" />
+        <div className="about__left">
+          <div className="about__me">
+            <div className="about__me-img">
+              <img src={ME} alt="About Me" />
+            </div>
           </div>
         </div>
 
-        <div className="about__content">
-          <div className="about__cards">
-            <article className="about__card">
-              <AiOutlineExperiment className="about__icon" />
-              <h5>Experience</h5>
-              <small>{experiment_years}+ Years Working</small>
-            </article>
+        <div className="about__right">
+          <div className="about__content">
+            <div className="about__cards">
+              <article className="about__card">
+                <AiOutlineExperiment className="about__icon" />
+                <h5>Experience</h5>
+                <small>{experiment_years}+ Years Working</small>
+              </article>
 
-            <article className="about__card">
-              <AiFillFolderOpen className="about__icon" />
-              <h5>Projects</h5>
-              <small>{projects_number}+ Projects</small>
-            </article>
+              <article className="about__card">
+                <AiFillFolderOpen className="about__icon" />
+                <h5>Projects</h5>
+                <small>{projects_number}+ Projects</small>
+              </article>
 
-            <article className="about__card">
-              <BiBuildingHouse className="about__icon" />
-              <h5>Worked For</h5>
-              <small>{worked_for}+ Companies</small>
-            </article>
+              <article className="about__card">
+                <BiBuildingHouse className="about__icon" />
+                <h5>Worked For</h5>
+                <small>{worked_for}+ Companies</small>
+              </article>
+            </div>
+
+            <p>
+              Passion with code and beautiful website. I may be not good in many
+              thing but will try to finish anything as well as i can !
+            </p>
+            <a href="#about" className="btn btn-primary">
+              See More
+            </a>
           </div>
-
-          <p>
-            Passion with code and beautiful website. I may be not good in many
-            thing but will try to finish anything as well as i can
-          </p>
-          <a href="#about" className="btn btn-primary">
-            See More
-          </a>
         </div>
       </div>
     </div>
